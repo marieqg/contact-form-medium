247def903a0a92b2978261696e8dcad5b7745121
marieqg <mariequittelier@gmail.com>
2019-12-22 15:40:30 +0100

    5. THE function : sendEmail.js

--- src/lambda/sendEmail.js
+++ src/lambda/sendEmail.js
@@ -1,14 +1,31 @@
-// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
+// Gatsby settings for the environment variables
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// Connect to our Mailgun API
+const mailgun = require("mailgun-js")
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+})
+
+// Our Netlify function
 export function handler(event, context, callback) {
   let data = JSON.parse(event.body)
-  callback(null, {
-    statusCode: 200,
-    // That's the status code that will be return when the function is executed to indicates that everything is OK
-    body: JSON.stringify({
-      msg: "Hello, World! ",
-      event: event,
-      context: context,
-      data: data,
-    }),
+  let { name, email, message } = data
+  let mailOptions = {
+    from: `${name} <${email}>`,
+    to: process.env.MY_EMAIL_ADDRESS,
+    replyTo: email,
+    text: `${message}`,
+  }
+
+  // Our MailGun code
+  mg.messages().send(mailOptions, function(error, body) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    })
   })
 }
