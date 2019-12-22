45fb61a9a7c02fa9db49edc00475be4ddff965bd
marieqg <mariequittelier@gmail.com>
2019-12-22 11:18:40 +0100

    MVP Function Hello

---
+++ src/lambda/hello.js
@@ -0,0 +1,10 @@
+// For more info, check https://www.netlify.com/docs/functions/#javascript-lambda-functions
+export function handler(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    // That's the status code that will be return when the function is executed to indicates that everything is OK
+    body: JSON.stringify({
+      msg: "Hello, World! ",
+    }),
+  })
+}
