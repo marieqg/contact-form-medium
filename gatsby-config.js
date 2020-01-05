cda23f8037940c3a1e6475928de2afdc5292554f
marieqg <mariequittelier@gmail.com>
2020-01-05 17:46:23 +0100

    install material UI & set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,12 +14,14 @@ module.exports = {
       })
     )
   },
+
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-material-ui`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
