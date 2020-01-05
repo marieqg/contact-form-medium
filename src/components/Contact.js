17073c80701abd3f1b52d546dc004cda747ba911
marieqg <mariequittelier@gmail.com>
2020-01-05 18:47:22 +0100

    3. Store the values typed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,15 +3,46 @@ import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 
 export default class Contact extends React.Component {
-  state = {}
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  handleChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+    const statesToUpdate = {}
+    statesToUpdate[name] = value
+
+    this.setState(statesToUpdate)
+  }
 
   render() {
     return (
       <div>
         <form noValidate autoComplete="off">
-          <TextField id="standard-basic" label="Name" />
-          <TextField id="standard-basic" label="Email" />
-          <TextField id="standard-basic" label="Message" />
+          <TextField
+            id="standard-basic"
+            label="Name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            name="name"
+          />
+          <TextField
+            id="standard-basic"
+            label="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            name="email"
+          />
+          <TextField
+            id="standard-basic"
+            label="Message"
+            value={this.state.message}
+            onChange={this.handleChange}
+            name="message"
+          />
           <Button variant="contained" color="primary">
             Submit
           </Button>
