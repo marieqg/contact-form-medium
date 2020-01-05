b22a9ba885445709093af0781c2181bba1e88f70
marieqg <mariequittelier@gmail.com>
2020-01-05 18:06:28 +0100

    2. Create a non functioning form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,22 @@
 import React from "react"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
 
 export default class Contact extends React.Component {
   state = {}
 
   render() {
-    return <div>HELLO</div>
+    return (
+      <div>
+        <form noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Name" />
+          <TextField id="standard-basic" label="Email" />
+          <TextField id="standard-basic" label="Message" />
+          <Button variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+      </div>
+    )
   }
 }
